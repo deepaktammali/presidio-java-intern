ed4f44ab3d21a28a930c7239ab59b48584e069a0
unknown <deepak.tammali627@gmail.com>
2021-03-10 14:47:30 +0530

    some problems solved

--- day3/src/corejavatest/IfElseExpressions.java
+++ day3/src/corejavatest/IfElseExpressions.java
@@ -1,5 +1,25 @@
 package corejavatest;
 
 public class IfElseExpressions {
-
+	public static void main(String[] args) {
+//		int i=0; 
+//		if(i) { 
+//		        System.out.println("Hello"); 
+//		        } 
+		
+		boolean b=true; 
+		boolean b2=true; 
+		if(b==b2) { 
+		        System.out.println("So true"); 
+		        }
+		
+		int i=1; 
+		int j=2; 
+		if(i==1|| j==2) 
+		        System.out.println("OK");
+//		i=1; 
+//		j=2; 
+//		if(i==1 &| j==2) 
+//		        System.out.println("OK");
+	}
 }
