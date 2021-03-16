536113dcd88cdaf3139dc68bc3b9a73e9fc18a8e
unknown <deepak.tammali627@gmail.com>
2021-03-16 18:03:24 +0530

    v2 of corruption handling problem

--- day7/src/concepts/HandlingCorruption.java
+++ day7/src/concepts/HandlingCorruption.java
@@ -2,33 +2,34 @@ package concepts;
 
 public class HandlingCorruption {
 	public static void main(String[] args) {
-		Government.SevaKendra sevaKendra = new Government().new SevaKendra();
-		
+		SevaKendra sevaKendra = new SevaKendra();
+
 		DeathCertificateBroker deathCertificateBroker = new DeathCertificateBroker(sevaKendra);
 		HotelPermitBroker hotelPermitBroker = new HotelPermitBroker(sevaKendra);
 		FoodAuthorisationBroker foodAuthorisationBroker = new FoodAuthorisationBroker(sevaKendra);
-		
+
 		deathCertificateBroker.submit();
 		hotelPermitBroker.submit();
 		foodAuthorisationBroker.submit();
-		
+
 	}
 }
-
 
 // brokers
 
-abstract class Broker{	
-	protected Government.SevaKendra  sevaKendra;
-	public Broker(Government.SevaKendra sevaKendra) {
+abstract class Broker {
+	protected SevaKendra sevaKendra;
+
+	public Broker(SevaKendra sevaKendra) {
 		this.sevaKendra = sevaKendra;
 	}
+
 	abstract void submit();
 }
 
-class DeathCertificateBroker extends Broker{
+class DeathCertificateBroker extends Broker {
 
-	public DeathCertificateBroker(Government.SevaKendra sevaKendra) {
+	public DeathCertificateBroker(SevaKendra sevaKendra) {
 		super(sevaKendra);
 	}
 
@@ -36,12 +37,12 @@ class DeathCertificateBroker extends Broker{
 	void submit() {
 		this.sevaKendra.deathCertificateService();
 	}
-	
+
 }
 
-class HotelPermitBroker extends Broker{
+class HotelPermitBroker extends Broker {
 
-	public HotelPermitBroker(Government.SevaKendra sevaKendra) {
+	public HotelPermitBroker(SevaKendra sevaKendra) {
 		super(sevaKendra);
 	}
 
@@ -49,12 +50,12 @@ class HotelPermitBroker extends Broker{
 	void submit() {
 		this.sevaKendra.hotelConstructionPermitService();
 	}
-	
+
 }
 
-class FoodAuthorisationBroker extends Broker{
+class FoodAuthorisationBroker extends Broker {
 
-	public FoodAuthorisationBroker(Government.SevaKendra sevaKendra) {
+	public FoodAuthorisationBroker(SevaKendra sevaKendra) {
 		super(sevaKendra);
 	}
 
@@ -62,102 +63,92 @@ class FoodAuthorisationBroker extends Broker{
 	void submit() {
 		this.sevaKendra.foodAuthorisingService();
 	}
-	
+
 }
 
+//Seva Department
 
+class SevaKendra {
+	private PoliceDepartment policeDepartment;
+	private Hospital hospital;
+	private ConstructionDepartment constructionDepartment;
+	private FoodSafetyDepartment foodSafetyDepartment;
 
-class Government {
+	public SevaKendra() {
+		policeDepartment = new PoliceDepartment();
+		hospital = new Hospital();
+		constructionDepartment = new ConstructionDepartment();
+		foodSafetyDepartment = new FoodSafetyDepartment();
+	}
 
-//	Seva Department
+	// services
 
-	public class SevaKendra{
-		private PoliceDepartment policeDepartment;
-		private Hospital hospital;
-		private ConstructionDepartment constructionDepartment;
-		private FoodSafetyDepartment foodSafetyDepartment;
-		
+	private abstract class Service {
+		abstract void execute();
+	}
 
-		public SevaKendra() {
-			policeDepartment = new PoliceDepartment();
-			hospital = new Hospital();
-			constructionDepartment = new ConstructionDepartment();
-			foodSafetyDepartment = new FoodSafetyDepartment();	
+	private class DeathCertService extends Service {
+
+		@Override
+		void execute() {
+			hospital.signDeathCert();
+			policeDepartment.verifyDeathCert();
 		}
-		
-		// services
-		
-		private abstract class Service {
-			abstract void execute();
-		}
+	}
 
-		private class DeathCertService extends Service {
+	private class HotelOpeningService extends Service {
+		@Override
+		void execute() {
+			constructionDepartment.permitHotelConstruction();
+		}
+	}
 
-			@Override
-			void execute() {
-				hospital.signDeathCert();
-				policeDepartment.verifyDeathCert();
-			}
-		}
-
-		private class HotelOpeningService extends Service {
-			@Override
-			void execute() {
-				constructionDepartment.permitHotelConstruction();
-			}
-		}
-
-		private class FoodTestingService extends Service {
-			@Override
-			void execute() {
-				foodSafetyDepartment.authoriseProduct();
-			}
-		}
-		
-		
-		public void hotelConstructionPermitService() {
-			System.out.println("Applied for hotel permit.");
-			new HotelOpeningService().execute();
-		}
-		
-		public void deathCertificateService() {
-			System.out.println("Applied for death certificate.");
-			new DeathCertService().execute();
-		}
-		
-		public void foodAuthorisingService() {
-			System.out.println("Applied for food authorisation.");
+	private class FoodTestingService extends Service {
+		@Override
+		void execute() {
 			foodSafetyDepartment.authoriseProduct();
 		}
 	}
-	
-	
-	
-// Government Organisations
 
-	private class ConstructionDepartment {
-		public void permitHotelConstruction() {
-			System.out.println("	Permitted Hotel Construction.");
-		}
+	public void hotelConstructionPermitService() {
+		System.out.println("Applied for hotel permit.");
+		new HotelOpeningService().execute();
 	}
 
-	private class Hospital {
-		public void signDeathCert() {
-			System.out.println("	Signed Death Certificate");
-		}
+	public void deathCertificateService() {
+		System.out.println("Applied for death certificate.");
+		new DeathCertService().execute();
 	}
 
-	private class PoliceDepartment {
-		public void verifyDeathCert() {
-			System.out.println("	Death Certificate Verified.");
-		}
+	public void foodAuthorisingService() {
+		System.out.println("Applied for food authorisation.");
+		foodSafetyDepartment.authoriseProduct();
 	}
+}
 
-	private class FoodSafetyDepartment {
-		public void authoriseProduct() {
-			System.out.println("	Product tested.");
-			System.out.println("	Product permitted to be supplied to people");
-		}
+//Government Organisations
+
+class ConstructionDepartment {
+	public void permitHotelConstruction() {
+		System.out.println("	Permitted Hotel Construction.");
 	}
+}
 
+class Hospital {
+	public void signDeathCert() {
+		System.out.println("	Signed Death Certificate");
+	}
+}
+
+class PoliceDepartment {
+	public void verifyDeathCert() {
+		System.out.println("	Death Certificate Verified.");
+	}
+}
+
+class FoodSafetyDepartment {
+	public void authoriseProduct() {
+		System.out.println("	Product tested.");
+		System.out.println("	Product permitted to be supplied to people");
+	}
 }
