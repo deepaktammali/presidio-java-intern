79d73667a92e7f12b3ff9d90da86fc7ca5688958
unknown <deepak.tammali627@gmail.com>
2021-03-25 16:22:37 +0530

    added observer demo

--- day14/src/concepts/ObserverDemo.java
+++ day14/src/concepts/ObserverDemo.java
@@ -2,56 +2,88 @@ package concepts;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
+
 public class ObserverDemo {
 	public static void main(String[] args) {
-		FireAlarm shakthi=new FireAlarm();
+		FireAlarm shakthi = new FireAlarm();
 		shakthi.addObserver(new Student());
 		shakthi.addObserver(new Teacher());
-		shakthi.setFire();		
+		shakthi.setFire();
 	}
 }
-class ThreadedObservable extends Observable{
-	@Override
-		public synchronized void addObserver(Observer o) {
-			// TODO Auto-generated method stub
-			super.addObserver(o);
-		}
-	@Override
-		public void notifyObservers(Object arg) {
-			// TODO Auto-generated method stub
 
-				super.notifyObservers(arg);
-
-		}
+interface ThreadedObserver{
+	public void update(ThreadedObservable o,Object arg);
 }
-class FireAlarm extends ThreadedObservable{
+
+abstract class ThreadedObservable{
+
+	private Vector<ThreadedObserver> observers;
+
+	public ThreadedObservable() {
+		observers = new Vector<ThreadedObserver>();
+	}
+	
+	public synchronized void deleteObserver(ThreadedObserver o) {
+		if (observers.contains(o)) {
+			observers.remove(o);
+		}
+	}
+	
+	public synchronized void addObserver(ThreadedObserver o) {
+		observers.add(o);
+	}
+
+	public void notifyObservers(Object arg) {
+		
+		synchronized(this) {
+			
+		}
+		
+		ExecutorService cachedPool = Executors.newCachedThreadPool();
+		observers.forEach((o) -> {
+			cachedPool.submit((Runnable) () -> {
+				o.update(this,(Object)arg);
+			});
+		});
+	}
+}
+
+
+
+class FireAlarm extends ThreadedObservable {
 	public void setFire() {
-		setChanged();
-		notifyObservers("fire in the mountain run run run..............");//signal is sent
+		notifyObservers("fire in the mountain run run run..............");// signal is sent
 	}
 }
-class Student implements Observer{
+
+class Student implements ThreadedObserver {
 	@Override
-	public void update(Observable o, Object arg) {
-		new Thread(()->{
-			runAway((String)arg);
-		}).start();
+	public void update(ThreadedObservable o, Object arg) {
+		runAway((String) arg);
 	}
+
 	public void runAway(String msg) {
-		System.out.println("students running..........away........"+msg);
+		System.out.println("students running..........away........" + msg);
 	}
 }
-class Teacher implements Observer{
+
+class Teacher implements ThreadedObserver {
 	@Override
-	public void update(Observable o, Object arg) {
-		new Thread(()->{
-			runAway((String)arg);
-		}).start();
+	public void update(ThreadedObservable o, Object arg) {
+		runAway((String) arg);
 	}
+
 	public void runAway(String msg) {
 		System.out.println("Teacher..thinking.................");
-		try {Thread.sleep(10000);}catch(Exception e) {}
-		System.out.println("teacher running...........away........."+msg);
+		try {
+			Thread.sleep(10000);
+		} catch (Exception e) {
+		}
+		System.out.println("teacher running...........away........." + msg);
 	}
 }
